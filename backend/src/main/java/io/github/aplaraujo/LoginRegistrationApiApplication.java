e50bd21f790574fb2faab3f1f3ec1314cce2369c
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-21 14:59:49 -0300

    first commit

---
+++ backend/src/main/java/io/github/aplaraujo/LoginRegistrationApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.aplaraujo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegistrationApiApplication.class, args);
+	}
+
+}
